41b44218bbd5cde701c21a2f33d75b6136abee33
diegoarff <diegorinconf1@gmail.com>
2023-11-22 19:39:17 -0400

    update services

--- service3/src/read.js
+++ service3/src/read.js
@@ -1,18 +1,24 @@
 const pool = require("../dbConnection");
-const getPCData = require ('../pcData');
-async function read (call, callback){
+const getPCData = require("../pcData");
+async function read(call, callback) {
+  const tab = `tab${Math.floor(Math.random() * 3) + 1}`;
+  const query = `SELECT * from ${tab};`;
 
-const query = `SELECT * from tab${Math.floor(Math.random() * 3)+1};`;
-
-try {
-const data = await pool.query(query);
-const pcData = await getPCData()
-console.log(data.rows)
-console.log(pcData)
-callback(null, {data: data.rows, freeMem: pcData.ramFree, freeCPU: pcData.cpuFree })
-} catch (error) {
+  try {
+    const data = await pool.query(query);
+    const pcData = await getPCData();
+    console.log(data.rows);
+    console.log(tab);
+    console.log(pcData);
+    callback(null, {
+      data: data.rows,
+      freeMem: pcData.ramFree,
+      freeCPU: pcData.cpuFree,
+      table: tab,
+    });
+  } catch (error) {
     callback(error);
-}
+  }
 }
 
-module.exports = read
+module.exports = read;
